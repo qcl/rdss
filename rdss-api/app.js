4f30ac34d169d8c7ee464e99fcabe14ffdae752e
Qing-Cheng Li <qc.linux@gmail.com>
2016-04-18 00:12:34 +0800

    Fix module

--- rdss-api/app.js
+++ rdss-api/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
